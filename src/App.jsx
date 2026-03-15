1dd4160192aaade8c4c3cbdba592a9ee736f383c
Yousef Hendy <yousefhendy239@gmail.com>
2026-03-15 12:05:36 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+	return (
+		<div className="min-h-screen flex flex-col bg-black text-white/95 items-center justify-center text-2xl font-bold text-center">
+			<img
+				className="size-40"
+				src="https://res.cloudinary.com/dltj8bim0/image/upload/v1761060580/logo_kukwt0.png"
+				alt=""
+			/>
+			<p>Hello Vite + React + TailwindCSS!</p>
+		</div>
+	);
+};
+
+export default App;
